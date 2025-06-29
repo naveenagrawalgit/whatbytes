dad6651b196047031c079a71b173daf247bf68ad
naveen <naveenagrawal1008@gmail.com>
2025-06-29 19:04:45 +0530

    5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,19 @@
-import { SideBar } from '../components/SideBar';
+// app/page.tsx
+import { Suspense } from 'react';
 import Card from '../components/card';
-import { ProductCategory } from '../../data/types';
+import { SideBar } from '../components/SideBar';
 
 export default function Home({
   searchParams
 }: {
-  searchParams: {
-    category?: ProductCategory;
-    maxPrice?: string;
-    search?: string;
-  }
+  searchParams: { [key: string]: string | string[] | undefined }
 }) {
   return (
     <div className="flex pt-12 gap-4 px-4">
       <div className="w-1/4 sticky top-12 h-fit">
-        <SideBar />
+        <Suspense fallback={<div className="p-4 bg-white rounded-lg">Loading filters...</div>}>
+          <SideBar />
+        </Suspense>
       </div>
       <div className="w-3/4">
         <Card searchParams={searchParams} />
