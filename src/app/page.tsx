2bbc9a437cfdef9bd2470bef4d1b0ab22c67c461
naveen <naveenagrawal1008@gmail.com>
2025-06-29 18:40:59 +0530

    3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
-
 import { SideBar } from '../components/SideBar';
 import Card from '../components/card';
 import { ProductCategory } from '../../data/types';
 
-interface HomeProps {
+export default function Home({
+  searchParams
+}: {
   searchParams: {
     category?: ProductCategory;
     maxPrice?: string;
     search?: string;
-  };
-}
-// @ts-expect-error - Disabling type checking for deployment
-export default function Home({ searchParams }: HomeProps) {
+  }
+}) {
   return (
     <div className="flex pt-12 gap-4 px-4">
       <div className="w-1/4 sticky top-12 h-fit">
